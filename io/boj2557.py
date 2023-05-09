bd9c43e7c562fac4fccea46a3657158d04aee584
ghddbwns9808 <ghddbwns9808@gmail.com>
2023-05-10 01:16:00 +0900

    [BOJ | IO] 10952: A+B - 5

--- io/boj2557.py
+++ io/boj2557.py
@@ -1,6 +1,5 @@
 while True:
-    try:
-        A, B = map(int, input().split())
-        print(A+B)
-    except:
+    A, B = map(int, input().split())
+    if A == 0 and B == 0:
         break
+    print(A+B)
